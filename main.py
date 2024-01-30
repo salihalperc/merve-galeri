1b3239d293c1037f09f689d2957eb7fc05054aa9
salihalperc <salihalpercetin@gmail.com>
2024-01-30 23:27:54 +0300

    Trying to add authentication

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
-from flask import Flask, render_template
-
+import os
+from flask import Flask, render_template, redirect, url_for, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor
+from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get("FLASK_KEY")
+ckeditor = CKEditor(app)
+Bootstrap5(app)
 
-@app.route("/")
+f_email = os.environ.get("EMAIL")
+f_password = os.environ.get("PASSWORD")
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Let Me In!")
+
+
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    form = LoginForm()
+    if form.validate_on_submit() and request.method=="POST":
+        if form.email.data == f_email and form.email.data == f_password:
+            return redirect(url_for("galeri"))
 
+    return render_template("login.html", form=form)
+
+@app.route("/galeri")
+def galeri():
+    return render_template("index.html")
 
 
 if __name__=="__main__":
